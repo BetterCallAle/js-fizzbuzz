5a3e2e6ae77e2a9b7c9f58bc6205cf0634a3b2f2
BetterCallAle <dap22091998@gmail.com>
2022-10-10 15:33:05 +0200

    added condition for % 3 or % 5 and % 3 && % 5

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 //Scrivo un ciclo che vada da 1 a 100
 for (let i = 1; i <= 100; i++){
-    console.log(i);
+
+    //SE un numero è multiplo di 3
+        //la scritta sarà FIZZ
+    //ALTRIMENTI SE un numero è multiplo di 5
+        //la scritta sarà BUZZ
+    let fizzBuzz = i
+    if (i % 3 === 0) {
+        fizzBuzz="FIZZ"
+    } else if (i % 5 === 0) {
+        fizzBuzz ="BUZZ"
+    }
+
+    //SE il numero è sia divisibile per 3 che per 5
+        //la scritta sarà FIZZBUZZ
+    if(i % 3 === 0 && i % 5 === 0) {
+        fizzBuzz="FIZZBUZZ"
+    }
+
+    console.log(i, fizzBuzz);
 }
